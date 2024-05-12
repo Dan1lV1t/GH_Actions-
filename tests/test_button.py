cccd1cd3327b000f39b905a7cc75292b1b99d6b2
Danylo <vdanil2012@gmail.com>
2024-05-12 20:16:34 +0300

    Workflow two tests

--- tests/test_button.py
+++ tests/test_button.py
@@ -18,3 +18,9 @@ def browser():
 def test_button(browser):
     browser.get("https://www.qa-practice.com/elements/button/simple")
     assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
+
+
+def test_button_second(browser):
+    browser.get("https://www.qa-practice.com/elements/button/like_a_button")
+    assert browser.find_element(By.PARTIAL_LINK_TEXT, 'Click').is_displayed()
+
